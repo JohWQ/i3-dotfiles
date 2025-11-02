bccf587cc7600c662b664090e5f78f3b213cd0ce
JohWQ <johnwillypaasche@gmail.com>
2025-11-02 01:03:21 +0100

    bugfix

--- .config/scripts/power/poweroff.sh
+++ .config/scripts/power/poweroff.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 playerctl pause
 xrandr --output DP-4 --off
-sudo umount -f /media/WINDOWS
-sudo umount -f /media/SSD1TB
-sudo umount -f /media/NTFSHDD4TB
-sudo umount -f /media/EXT4HDD4TB
-sudo umount -f /mnt/4TB
-#systemctl stop --user xdg-desktop-portal-gtk.service
-#pkill baobab
+umount /media/NTFSHDD4TB
+pkill baobab
 pkill mpd
 pkill dunst
 pkill alacritty
