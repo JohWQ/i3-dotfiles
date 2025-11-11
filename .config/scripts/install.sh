e6e422797b8c1e87aea1836c83d12873ef8bea69
JohWQ <johnwillypaasche@gmail.com>
2025-11-11 01:28:53 +0100

    misc

--- .config/scripts/install.sh
+++ .config/scripts/install.sh
@@ -5,12 +5,18 @@ sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-releas
 sudo dnf copr enable alternateved/i3status-rust
 sudo dnf update
 
+# Rustup:
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sleep 1
+rustup update
+# Packages from repos:
 echo "Installing packages:"
 sudo dnf group install multimedia "development-tools"
 sudo dnf install \
 i3status-rust \
 gcc \
 clang \
+cmake \
 python3 \
 meson \
 make \
@@ -22,6 +28,8 @@ jq \
 fontconfig \
 liberation-mono-fonts \
 cascadia-code-nf-fonts \
+inih-devel \
+scdoc \
 libev-devel \
 libjpeg-turbo-devel \
 libXinerama \
@@ -67,6 +75,7 @@ obs-studio
 echo "Installing various packages:"
 cd $HOME
 mkdir i3-dotfiles-install && cd i3-dotfiles-install
+cargo install --force --git https://github.com/sxyazi/yazi.git yazi-build
 git clone https://github.com/Raymo111/i3lock-color.git && cd i3lock-color && sudo ./install-i3lock-color.sh
 cd $HOME/i3-dotfiles-install
 git clone https://github.com/yeyushengfan258/Polarnight-Cursors.git && cd Polarnight-Cursors
@@ -77,11 +86,19 @@ meson setup build
 meson compile -C build && cd build
 sudo meson install
 cd $HOME/i3-dotfiles-install
+git clone https://github.com/GermainZ/xdg-desktop-portal-termfilechooser.git && cd xdg-desktop-portal-termfilechooser
+meson build && sudo ninja -C build install
+cd $HOME/i3-dotfiles-install
+git clone https://github.com/boydaihungst/org.freedesktop.FileManager1.common && cd org.freedesktop.FileManager1.common
+meson setup build --reconfigure
+sudo ninja -C build install
+cd $HOME/i3-dotfiles-install
 wget https://raw.githubusercontent.com/betterlockscreen/betterlockscreen/main/install.sh -O - -q | sudo bash -s system
 wget https://github.com/erebe/greenclip/releases/download/v4.2/greenclip && sudo mv greenclip /usr/local/bin/
 curl https://raw.githubusercontent.com/GeorgeFilipkin/pulsemixer/master/pulsemixer > pulsemixer && chmod +x pulsemixer && sudo mv pulsemixer /usr/local/bin/
-cd $HOME/i3-dotfiles-install
 sudo rm -rf $HOME/i3-dotfiles-install/*
+
+
 
 
 echo "Clearing font cache:"
