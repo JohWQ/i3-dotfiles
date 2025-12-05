7cd587c8f688eebcc9f2afe20c360ec9c7ad0122
JohWQ <johnwillypaasche@gmail.com>
2025-12-05 22:47:58 +0100

    updated installer

--- .config/scripts/installer/install-dotfiles.sh
+++ .config/scripts/installer/install-dotfiles.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 
-mv $HOME/.bashrc{,.bak}
-mv $HOME/.gtkrc-2.0{,.bak}
-mv $HOME/.profile{,.bak}
-mv $HOME/.xprofile{,.bak}
-mv $HOME/.Xresources{,.bak}
-mv $HOME/.zshrc{,.bak}
-mv $HOME/.tmux/{,.bak}
-mv $HOME/xdg-desktop-portal-termfilechooser/{,.bak}
+mv $HOME/.bashrc $HOME/.bashrc.bak
+mv $HOME/.gtkrc-2.0 $HOME/.gtkrc-2.0.bak
+mv $HOME/.profile $HOME/.profile.bak
+mv $HOME/.xprofile $HOME/.xprofile.bak
+mv $HOME/.Xresources $HOME/.Xresources.bak
+mv $HOME/.zshrc $HOME/.zshrc.bak
+mv $HOME/.tmux $HOME/.tmux.bak
+mv $HOME/xdg-desktop-portal-termfilechooser $HOME/xdg-desktop-portal-termfilechooser.bak
 
-mv $HOME/.config/pulsemixer.cfg{,.bak}
-mv $HOME/.config/alacritty/{,.bak}
-mv $HOME/.config/betterlockscreen/{,.bak}
-mv $HOME/.config/dunst/{,.bak}
-mv $HOME/.config/fastfetch/{,.bak}
-mv $HOME/.config/gtk-3.0/{,.bak}
-mv $HOME/.config/gtk-4.0/{,.bak}
-mv $HOME/.config/i3/{,.bak}
-mv $HOME/.config/i3status-rust/{,.bak}
-mv $HOME/.config/mpd/{,.bak}
-mv $HOME/.config/nvim/{,.bak}
-mv $HOME/.config/org.freedesktop.FileManager1.common/{,.bak}
-mv $HOME/.config/picom/{,.bak}
-mv $HOME/.config/rmpc/{,.bak}
-mv $HOME/.config/scripts/{,.bak}
-mv $HOME/.config/tmux/{,.bak}
-mv $HOME/.config/xdg-desktop-portal/{,.bak}
-mv $HOME/.config/yazi/{,.bak}
+mv $HOME/.config/pulsemixer.cfg $HOME/.config/pulsemixer.cfg.bak
+mv $HOME/.config/alacritty $HOME/.config/alacritty.bak
+mv $HOME/.config/betterlockscreen $HOME/.config/betterlockscreen.bak
+mv $HOME/.config/dunst $HOME/.config/dunst.bak
+mv $HOME/.config/fastfetch $HOME/.config/fastfetch.bak
+mv $HOME/.config/gtk-3.0 $HOME/.config/gtk-3.0.bak
+mv $HOME/.config/gtk-4.0 $HOME/.config/gtk-4.0.bak
+mv $HOME/.config/i3 $HOME/.config/i3.bak
+mv $HOME/.config/i3status-rust $HOME/.config/i3status-rust.bak
+mv $HOME/.config/mpd $HOME/.config/mpd.bak
+mv $HOME/.config/nvim $HOME/.config/nvim.bak
+mv $HOME/.config/org.freedesktop.FileManager1.common $HOME/.config/org.freedesktop.FileManager1.common.bak
+mv $HOME/.config/picom $HOME/.config/picom.bak
+mv $HOME/.config/rmpc $HOME/.config/rmpc.bak
+mv $HOME/.config/scripts $HOME/.config/scripts.bak
+mv $HOME/.config/tmux $HOME/.config/tmux.bak
+mv $HOME/.config/xdg-desktop-portal $HOME/.config/xdg-desktop-portal.bak
+mv $HOME/.config/yazi $HOME/.config/yazi.bak
 
 mv $HOME/i3-dotfiles/.zshrc-sample $HOME/i3-dotfiles/.zshrc
 mv $HOME/i3-dotfiles/.config/i3/scripts/new-workspace-sample.sh $HOME/i3-dotfiles/.config/i3/scripts/new-workspace.sh
